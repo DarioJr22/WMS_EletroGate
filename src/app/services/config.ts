23788190aa7d29825111b670baccf63c37c9ec2a
dario.junior <dariojr1233@gmail.com>
2024-05-20 06:57:08 -0300

    Finalização Ajustes 1.3

--- src/app/services/config.ts
+++ src/app/services/config.ts
@@ -1,12 +1,12 @@
 export enum Config {
   //PROD
- /*  UrlLogin = 'https://www.bling.com.br/Api/v3/oauth/authorize?response_type=code&client_id=0f440f3f566895942ac1b12c0cbe6693a915a46a&state=81cb4eef2e13162fd254e2584f3651cc',
+  UrlLogin = 'https://www.bling.com.br/Api/v3/oauth/authorize?response_type=code&client_id=0f440f3f566895942ac1b12c0cbe6693a915a46a&state=81cb4eef2e13162fd254e2584f3651cc',
   clientId = '0f440f3f566895942ac1b12c0cbe6693a915a46a',
-  secretId = '914798832f446615d1db189db8b4a616b0b75cf2ed2df2dd8cc6da90d0f7'*/
+  secretId = '914798832f446615d1db189db8b4a616b0b75cf2ed2df2dd8cc6da90d0f7'
   //DEV
-  UrlLogin = 'https://www.bling.com.br/Api/v3/oauth/authorize?response_type=code&client_id=76ae3fe74a8b292b1bbbedb9ae2e0cab819875bb&state=b09c95e4f833c3c65ca8e0f1e4018c71',
+  /* UrlLogin = 'https://www.bling.com.br/Api/v3/oauth/authorize?response_type=code&client_id=76ae3fe74a8b292b1bbbedb9ae2e0cab819875bb&state=b09c95e4f833c3c65ca8e0f1e4018c71',
   clientId = '76ae3fe74a8b292b1bbbedb9ae2e0cab819875bb',
-  secretId = '42cb4845f35d1b16a64de85a3237874dbe7e8f61e2531c7d95f2d822aad2'
+  secretId = '42cb4845f35d1b16a64de85a3237874dbe7e8f61e2531c7d95f2d822aad2' */
 }
 
 
